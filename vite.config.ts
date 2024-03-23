14ce04d02c6b3c38822a961b2b1bbc922f168f07
mst-mkt <s1f102303057@iniad.org>
2024-03-24 04:53:04 +0900

    chore: remove unnecessary comment in `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import CSSExtractorPlugin from '@master/css-extractor.vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), CSSExtractorPlugin()],
 })
