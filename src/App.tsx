7b7a698ca68b93eb91fc0486f777991c9f60c988
mst-mkt <s1f102303057@iniad.org>
2024-03-24 06:01:43 +0900

    refactor: separate the processing around pyodide into hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { loadPyodide, type PyodideInterface } from 'pyodide'
-import { useEffect, useState } from 'react'
 import { useInput } from './hooks/useInput'
+import { usePyodide } from './hooks/usePyodide'
 
 const App = () => {
-  const [pyodide, setPyodide] = useState<PyodideInterface>()
-  const [shellResults, setShellResults] = useState<string[]>([])
-  const [isLoading, setIsLoading] = useState(true)
   const [inputText, handleInputChange, clearInput] = useInput('')
-
-  const execute = async () => {
-    if (pyodide === undefined) return
-    if (inputText === '') return
-
-    try {
-      const result = await pyodide.runPythonAsync(inputText)
-      setShellResults((prev) => [
-        ...prev,
-        `>>> ${inputText.trim()}`,
-        ...(result === '' ? [] : [result]),
-      ])
-    } catch (error) {
-      setShellResults((prev) => [...prev, `>>> ${inputText.trim()}`, `${error}`])
-    } finally {
-      clearInput()
-    }
-  }
-
-  useEffect(() => {
-    loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.25.0/full/' })
-      .then((pyodide) => setPyodide(pyodide))
-      .catch((error) => console.error(error))
-      .finally(() => setIsLoading(false))
-  }, [])
+  const { isLoading, shellResults, execute } = usePyodide({ execCallback: clearInput })
 
   if (isLoading) return <div>initializing...</div>
 
@@ -43,7 +15,7 @@ const App = () => {
         ))}
       </div>
       <textarea value={inputText} onChange={handleInputChange<HTMLTextAreaElement>} />
-      <button type="button" onClick={execute}>
+      <button type="button" onClick={() => execute(inputText)}>
         Execute
       </button>
     </div>
---
+++ src/hooks/usePyodide.ts
@@ -0,0 +1,42 @@
+import { loadPyodide, type PyodideInterface } from 'pyodide'
+import { useCallback, useEffect, useState } from 'react'
+
+type Option = {
+  execCallback?: () => void
+}
+
+export const usePyodide = ({ execCallback }: Option) => {
+  const [pyodide, setPyodide] = useState<PyodideInterface | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [shellResults, setShellResults] = useState<string[]>([])
+
+  const execute = useCallback(
+    async (inputText: string) => {
+      if (pyodide === null) return
+      if (inputText === '') return
+
+      try {
+        const result = await pyodide.runPythonAsync(inputText)
+        setShellResults((prev) => [
+          ...prev,
+          `>>> ${inputText.trim()}`,
+          ...(result === '' ? [] : [result]),
+        ])
+      } catch (error) {
+        setShellResults((prev) => [...prev, `>>> ${inputText.trim()}`, `${error}`])
+      } finally {
+        execCallback?.()
+      }
+    },
+    [pyodide, execCallback],
+  )
+
+  useEffect(() => {
+    loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.25.0/full/' })
+      .then((pyodide) => setPyodide(pyodide))
+      .catch((error) => console.error(error))
+      .finally(() => setIsLoading(false))
+  }, [])
+
+  return { isLoading, shellResults, execute }
+}
