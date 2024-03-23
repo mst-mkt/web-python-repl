db3be6cc416338881ecda087b4aa5fdb7ad30a10
mst-mkt <s1f102303057@iniad.org>
2024-03-24 04:52:47 +0900

    chore: set up CSS Extractor of master-css in `vite.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import CSSExtractorPlugin from '@master/css-extractor.vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), CSSExtractorPlugin()],
 })
