67a0f92cf74daf7cdb5e084c6f3960fe3e019d23
mst-mkt <s1f102303057@iniad.org>
2024-03-24 08:10:25 +0900

    add: adapt normalize css by `@master/normal.css`

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import '.virtual/master.css'
+import '@master/normal.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import '.virtual/master.css'
 
 // biome-ignore lint/style/noNonNullAssertion: #root is exist in index.html
 ReactDOM.createRoot(document.getElementById('root')!).render(
