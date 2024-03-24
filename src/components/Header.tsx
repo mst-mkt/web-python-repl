12f81e58e7848227e2f6e693f653a26d429ca031
mst-mkt <s1f102303057@iniad.org>
2024-03-24 09:11:17 +0900

    add: link title for GitHub icon link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,7 @@ export const Header = () => {
       </h1>
       <a
         href="http://github.com/mst-mkt/web-python-repl"
+        title='GitHub Repository: "mst-mkt/web-python-repl"'
         className="p:8 r:8 bg:#2222:hover ~background-color|.1s|ease-in-out"
       >
         <IconBrandGithubFilled size={20} color="#333" />
